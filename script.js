47e434e173cd5770bb70dc22b912400b3d179791
SietseAndringa <82322340+SietseAndringa@users.noreply.github.com>
2021-10-09 13:48:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy");
+let name = 'Sietse';
+console.log(name);
+// if kan niet gebruikt worden om een variable aan te maken, want deze term wordt al gebruikt door Javascript zelf als conditional statement
+console.log(4 + 4);
+console.log("4" + "4"); // twee strings bijelkaar optellen lukt niet, omdat de strings niet herkend worden als nummers
+console.log(5 + 15);
+console.log(20 - 10);
+console.log(5 * 10);
+console.log(30 / 10);
+console.log(56 % 15);
+let age = 36;
+console.log(typeof age);
+let ageTwo = "36";
+console.log(typeof ageTwo);
